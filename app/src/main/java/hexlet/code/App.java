9922c0e3620ca735d113edbf9301d66a95f476aa
Aliya <aakhisamova@gmail.com>
2024-05-03 23:19:33 +0400

    App update

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,28 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff",
+        description = "Compares two configuration files and shows a difference.")
+
+public class App implements Runnable {
+
+    @Option(names = {"-h", "--help"},
+            description = "Show this help message and exit.")
+    private boolean helpRequested;
+
+    @Option(names = {"-V", "--version"},
+            description = "Print version information and exit.")
+    private boolean versionRequested;
+
+    @Override
+    public void run() {
+        System.out.println("");
+    }
+
+    public static void main(String... args) {
+        System.out.println("");
     }
 }
