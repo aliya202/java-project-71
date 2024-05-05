d278290217325735afa86c79c27998b30a3a2afb
Aliya <aakhisamova@gmail.com>
2024-05-06 00:42:55 +0400

    App update

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,13 +1,26 @@
 package hexlet.code;
 
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.util.concurrent.Callable;
+
 @Command(name = "gendiff",
         description = "Compares two configuration files and shows a difference.")
 
-public class App implements Runnable {
+public class App implements Callable {
+
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"},
+            description = "output format [default: stylish]]")
+    private String format;
 
     @Option(names = {"-h", "--help"},
             description = "Show this help message and exit.")
@@ -18,11 +31,12 @@ public class App implements Runnable {
     private boolean versionRequested;
 
     @Override
-    public void run() {
-        System.out.println("");
+    public Integer call() {
+        return 0;
     }
 
     public static void main(String... args) {
-        System.out.println("");
+        CommandLine commandLine = new CommandLine(new App());
+        commandLine.usage(System.out);
     }
 }
