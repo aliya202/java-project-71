4eec118211fe35408693c90a4f27d4ad3f48f522
Aliya <aakhisamova@gmail.com>
2024-06-09 00:23:55 +0400

    added tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,59 +1,73 @@
 package hexlet.code;
 
+import static hexlet.code.Differ.generate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public final class DifferTest {
-    private static String resultStylish;
-    private static String resultPlain;
-    private static String resultJson;
-    private static String resultFile3Json;
+public class DifferTest {
+    private String readExpectedFilePath1 = "src/test/resources/files/file6.json";
+    private Path path1 = Paths.get(readExpectedFilePath1).toAbsolutePath().normalize();
+    private String readExpectedFilePath2 = "src/test/resources/files/file7.json";
+    private Path path2 = Paths.get(readExpectedFilePath2).toAbsolutePath().normalize();
+    private String readExpectedFilePath3 = "src/test/resources/files/file8.json";
+    private Path path3 = Paths.get(readExpectedFilePath3).toAbsolutePath().normalize();
 
-    private static Path getFilePath(String fileName) {
-        return Paths.get("src", "test", "resources", "files", fileName)
-                .toAbsolutePath().normalize();
+    @Test
+    public void testPlainJson() throws Exception {
+        String format = "plain";
+        String content1 = Files.readString(path2);
+        String readFilePath1 = "src/test/resources/files/file4.json";
+        String readFilePath2 = "src/test/resources/files/file5.json";
+        assertEquals(content1, generate(readFilePath1, readFilePath2, format));
     }
 
-    private static String getFileContent(String fileName) throws Exception {
-        Path filePath = getFilePath(fileName);
-        return Files.readString(filePath).trim();
+    @Test
+    public void testPlainYML() throws Exception {
+        String format = "plain";
+        String content1 = Files.readString(path2);
+        String readFilePath1 = "src/test/resources/files/file4.yml";
+        String readFilePath2 = "src/test/resources/files/file5.yml";
+        assertEquals(content1, generate(readFilePath1, readFilePath2, format));
     }
 
-    @BeforeAll
-    public static void beforeAll() throws Exception {
-        resultFile3Json = getFileContent("file3.json");
-        resultStylish = getFileContent("file6.json");
-        resultPlain = getFileContent("file7.json");
-        resultJson = getFileContent("file8.json");
+    @Test
+    public void testStylishJson() throws Exception {
+        String format = "stylish";
+        String content2 = Files.readString(path1);
+        String readFilePath1 = "src/test/resources/files/file4.json";
+        String readFilePath2 = "src/test/resources/files/file5.json";
+        assertEquals(content2, generate(readFilePath1, readFilePath2, format));
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"json", "yml"})
-    public void generateTest(String format) throws Exception {
-        String filePath1 = getFilePath("file1." + format).toString();
-        String filePath2 = getFilePath("file2." + format).toString();
-        System.out.println("File1 " + Differ.generate(filePath1, filePath2));
-        // Json and Yaml to stylish as default
-        assertThat(Differ.generate(filePath1, filePath2))
-                .isEqualTo(resultFile3Json);
-        // Json and Yaml to stylish
-        assertThat(Differ.generate(filePath1, filePath2, "stylish"))
-                .isEqualTo(resultFile3Json);
-        // Json and Yaml to plain
-        //assertThat(Differ.generate(filePath1, filePath2, "plain"))
-        //        .isEqualTo(resultFile3Json);
-        // Json and Yaml to json
-        //assertThat(Differ.generate(filePath1, filePath2, "json"))
-        //        .isEqualTo(resultJson);
+    @Test
+    public void testStylishYML() throws Exception {
+        String format = "stylish";
+        String content2 = Files.readString(path1);
+        String readFilePath1 = "src/test/resources/files/file4.yml";
+        String readFilePath2 = "src/test/resources/files/file5.yml";
+        assertEquals(content2, generate(readFilePath1, readFilePath2, format));
+    }
 
-        //String actualJson = Differ.generate(filePath1, filePath2, "json");
-        //JSONAssert.assertEquals(resultJson, actualJson, false);
+    @Test
+    public void testJsonJson() throws Exception {
+        String format = "json";
+        String content3 = Files.readString(path3);
+        String readFilePath1 = "src/test/resources/files/file4.json";
+        String readFilePath2 = "src/test/resources/files/file5.json";
+        assertEquals(content3, generate(readFilePath1, readFilePath2, format));
+    }
+
+    @Test
+    public void testJsonYML() throws Exception {
+        String format = "json";
+        String content3 = Files.readString(path3);
+        String readFilePath1 = "src/test/resources/files/file4.yml";
+        String readFilePath2 = "src/test/resources/files/file5.yml";
+        assertEquals(content3, generate(readFilePath1, readFilePath2, format));
     }
 }
