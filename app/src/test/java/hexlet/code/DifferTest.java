7c71b2be1371df10e272b53de449899d1200686e
Aliya <aakhisamova@gmail.com>
2024-06-09 00:37:37 +0400

    Test update

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -16,6 +16,10 @@ public class DifferTest {
     private Path path2 = Paths.get(readExpectedFilePath2).toAbsolutePath().normalize();
     private String readExpectedFilePath3 = "src/test/resources/files/file8.json";
     private Path path3 = Paths.get(readExpectedFilePath3).toAbsolutePath().normalize();
+    private String readExpectedFilePath4 = "src/test/resources/files/file3.json";
+    private Path path4 = Paths.get(readExpectedFilePath4).toAbsolutePath().normalize();
+    private String readExpectedFilePath5 = "src/test/resources/files/file3.yml";
+    private Path path5 = Paths.get(readExpectedFilePath5).toAbsolutePath().normalize();
 
     @Test
     public void testPlainJson() throws Exception {
@@ -69,5 +73,23 @@ public class DifferTest {
         String readFilePath1 = "src/test/resources/files/file4.yml";
         String readFilePath2 = "src/test/resources/files/file5.yml";
         assertEquals(content3, generate(readFilePath1, readFilePath2, format));
+    }
+
+    @Test
+    public void testStylish() throws Exception {
+        String format = "stylish";
+        String content4 = Files.readString(path4);
+        String readFilePath1 = "src/test/resources/files/file1.json";
+        String readFilePath2 = "src/test/resources/files/file2.json";
+        assertEquals(content4, generate(readFilePath1, readFilePath2, format));
+    }
+
+    @Test
+    public void testStylish2() throws Exception {
+        String format = "stylish";
+        String content5 = Files.readString(path5);
+        String readFilePath1 = "src/test/resources/files/file1.yml";
+        String readFilePath2 = "src/test/resources/files/file2.yml";
+        assertEquals(content5, generate(readFilePath1, readFilePath2, format));
     }
 }
